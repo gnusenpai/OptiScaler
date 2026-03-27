e794c84d39350d9f01c4d2c677e1618b2ce70384
cdozdil <cdozdil@gmail.com>
2026-03-27 10:37:10 +0300

    Fix clang

--- OptiScaler/framegen/xefg/XeFG_Dx12.cpp
+++ OptiScaler/framegen/xefg/XeFG_Dx12.cpp
@@ -718,7 +718,7 @@ bool XeFG_Dx12::Dispatch()
         LOG_WARN("Depth or Velocity is not ready, skipping");
         return false;
     }
-    
+
     auto& state = State::Instance();
 
     if (XeFGProxy::SetUiCompositionState() != nullptr &&
@@ -779,7 +779,6 @@ bool XeFG_Dx12::Dispatch()
             }
         }
     }
-
 
     if (!_haveHudless.has_value())
     {
